eb1bb8551af186326a23a30212495e0805966dab
atikur-rabbi <msawon94@gmail.com>
2022-12-06 00:36:43 +0600

    first commit

---
+++ stories/lwc.stories.js
@@ -0,0 +1,17 @@
+import { storiesOf } from '@storybook/html';
+import buildAndRegisterCustomElement from './utils/build-custom-element';
+import Header from 'c/header';
+
+buildAndRegisterCustomElement('c-header', Header);
+
+storiesOf('Header', module)
+    .add('basic', () => `
+        <div class="slds-p-around_medium">
+            <c-header label="Hello"><c-header>
+        <div class="slds-p-around_medium">    
+    `)
+    .add('demo', () => `
+        <div class="slds-p-around_medium">
+            <c-header label="Welcome"><c-header>
+        <div class="slds-p-around_medium">    
+    `);
